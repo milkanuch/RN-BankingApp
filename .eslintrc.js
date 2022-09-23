042691144e84159cba42e4df2f471d3479253afe
milkanuch <milkanic2@gmail.com>
2022-09-23 23:08:01 +0300

    feat: setup app linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,42 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'import'],
+  settings: {
+    'import/ignore': ['react-native', 'node_modules'],
+  },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.js'],
       rules: {
+        'react/react-in-jsx-scope': 'off',
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'react-native/no-unused-styles': 'error',
+        'react-native/split-platform-components': 'error',
+        'react-native/no-inline-styles': 'error',
+        'react-native/no-color-literals': 'error',
+        'react-native/no-raw-text': 'error',
+        'react-native/no-single-element-style-arrays': 'error',
+        'react/jsx-curly-brace-presence': [
+          'error',
+          { props: 'never', children: 'never' },
+        ],
+        'import/newline-after-import': ['error', { count: 1 }],
+        'import/order': [
+          'error',
+          {
+            groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+            'newlines-between': 'always-and-inside-groups',
+          },
+        ],
       },
     },
   ],
