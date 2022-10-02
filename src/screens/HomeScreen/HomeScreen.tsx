80cdffe1f22bc220008c8ccff8edecd4edb09b36
milkanuch <milkanic2@gmail.com>
2022-10-02 21:29:50 +0300

    feat: create home screen

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+import styles from './homeScreen.styles';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
