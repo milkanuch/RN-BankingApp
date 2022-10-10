c09ed3775160c78114f925d29474e5f63e2fc889
stepan103ua <stepan8877ua@gmail.com>
2022-10-10 19:39:36 +0300

    feat: added type for style properties

--- src/components/TabModeSelector/tabModeSelector.types.ts
+++ src/components/TabModeSelector/tabModeSelector.types.ts
@@ -1,8 +1,10 @@
+import { ViewStyle } from 'react-native';
+
 import { ITabModeSelectorItemData } from './TabModeSelectorItem/tabModeSelectorItem.types';
 
 export interface ITabModeSelectorProps {
   currentValue: any;
   setNewValue: (signInMode: any) => void;
   items: ITabModeSelectorItemData[];
-  style?: {};
+  style?: ViewStyle;
 }
