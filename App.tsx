8b81728a6be6355e5a4870d28830f69262d86287
Xaiodo <rostislavgovbak@gmail.com>
2022-10-14 16:28:52 +0300

    feat: splash screen setup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import SplashScreen from 'react-native-splash-screen';
 
 import AppNavigation from './src/navigation/AppNavigation/AppNavigation';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <AppNavigation />;
 };
 
--- android/app/src/main/java/com/bankingapp/MainApplication.java
+++ android/app/src/main/java/com/bankingapp/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.bankingapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
