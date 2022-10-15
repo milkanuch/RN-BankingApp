224dec28d3e316f243b74bcf57a2588513c57de2
milkanuch <milkanic2@gmail.com>
2022-10-15 20:58:31 +0300

    feat: add is user logged for switch between navigators

--- src/navigation/AppNavigation/AppNavigation.tsx
+++ src/navigation/AppNavigation/AppNavigation.tsx
@@ -1,11 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 
+import { useAppSelector } from '../../store';
+import { selectIsLogged } from '../../store/user/userSlice';
+
+import AuthStackNavigation from '../AuthStackNavigation/AuthStackNavigation';
 import BottomNavigation from '../BottomNavigation';
 
 const AppNavigation = () => {
+  const isLogged = useAppSelector(selectIsLogged);
+
   return (
     <NavigationContainer>
-      <BottomNavigation />
+      {isLogged ? <BottomNavigation /> : <AuthStackNavigation />}
     </NavigationContainer>
   );
 };
