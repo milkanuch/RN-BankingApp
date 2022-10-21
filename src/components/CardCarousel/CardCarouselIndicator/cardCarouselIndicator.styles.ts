072984f4b2c37eea8981f7c5529274ef48e196b0
stepan103ua <stepan8877ua@gmail.com>
2022-10-21 22:42:51 +0300

    feat: card carousel indicator component added

---
+++ src/components/CardCarousel/CardCarouselIndicator/cardCarouselIndicator.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
