094e590e161eb34d59cfae9026c69fed36f833b2
Xaiodo <rostislavgovbak@gmail.com>
2022-10-28 21:56:10 +0300

    feat: created pin code screen

---
+++ src/screens/PinCodeScreen/pinCodeScreen.styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from '../../constants/colors';
+import { responsiveHeight } from '../../constants/dimensions';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.darkGrey,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: responsiveHeight(4),
+  },
+});
+
+export default styles;
