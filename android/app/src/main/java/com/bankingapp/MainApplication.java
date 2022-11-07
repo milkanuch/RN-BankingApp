7e53d08fa6d195b3a8effb4768c240466dc3d1d0
milkanuch <milkanic2@gmail.com>
2022-11-07 15:05:06 +0200

    feat: configure gradle for react-native config

--- android/app/src/main/java/com/bankingapp/MainApplication.java
+++ android/app/src/main/java/com/bankingapp/MainApplication.java
@@ -13,6 +13,7 @@ import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.bankingapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -71,7 +73,7 @@ public class MainApplication extends Application implements ReactApplication {
     if (BuildConfig.DEBUG) {
       try {
         /*
-         We use reflection here to pick up the class that initializes Flipper,
+        We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
         Class<?> aClass = Class.forName("com.bankingapp.ReactNativeFlipper");
