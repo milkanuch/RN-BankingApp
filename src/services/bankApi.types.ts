9acd0e1abdfa11df8a0780e196109e04228743ec
stepan103ua <stepan8877ua@gmail.com>
2022-11-11 20:11:53 +0200

    feat: changed from query to mutation, optimized

--- src/services/bankApi.types.ts
+++ src/services/bankApi.types.ts
@@ -4,3 +4,13 @@ export interface UserParams
   extends Omit<User, 'isPremium' | 'isLogged' | 'isLoading'> {
   password: string;
 }
+
+export interface UserLoginParams {
+  ipn?: string;
+  phoneNumber?: string;
+  password: string;
+}
+export interface UserResponseParams {
+  jwt: string;
+  error: string | null | undefined;
+}
