292c80ce2c6f5cf813b18b76ef4f5226f1d8402b
Xaiodo <rostislavgovbak@gmail.com>
2022-11-12 19:24:50 +0200

    feat: create transfer screen

---
+++ src/screens/SuccesTransferScreen/succesTransferScreen.settings.ts
@@ -0,0 +1,4 @@
+export const succesTransferText = 'The transfer was completed successfully';
+export const deniedTransferText = 'The transfer was denied';
+export const succesTransferIcon = 'checkbox-marked-circle-outline';
+export const deniedTransferIcon = 'cancel';
