db90f0a53f6e9b1424bbc17939375e54584ee184
Xaiodo <rostislavgovbak@gmail.com>
2022-11-14 18:01:31 +0200

    feat: create profile stack navigation

---
+++ src/navigation/ProfileStackNavigation/profileStackNavigation.types.ts
@@ -0,0 +1,11 @@
+export enum ProfileStackScreenTypes {
+  Home = 'Home',
+  Transfer = 'Transfer',
+  IncomуExpense = 'IncomуExpense',
+  CardSettings = 'CardSettings',
+  Profile = 'Profile',
+}
+
+export type ProfileStackParamsList = {
+  [ProfileStackScreenTypes.Profile]: undefined;
+};
