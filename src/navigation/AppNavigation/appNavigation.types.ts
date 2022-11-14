52744d4147c08af2bcb472c4a6b4d07a7792b451
Xaiodo <rostislavgovbak@gmail.com>
2022-11-14 18:05:03 +0200

    feat: added bottom tab nav options

--- src/navigation/AppNavigation/appNavigation.types.ts
+++ src/navigation/AppNavigation/appNavigation.types.ts
@@ -2,10 +2,18 @@ import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 export enum BottomTabsScreenTypes {
   HomeStack = 'HomeStack',
+  MoneyTransfer = 'MoneyTransfer',
+  IncomeExpense = 'IncomуExpense',
+  CardSettings = 'CardSettings',
+  Profile = 'Profile',
 }
 
 export type BottomTabsParamsList = {
   [BottomTabsScreenTypes.HomeStack]: undefined;
+  [BottomTabsScreenTypes.MoneyTransfer]: undefined;
+  [BottomTabsScreenTypes.IncomeExpense]: undefined;
+  [BottomTabsScreenTypes.CardSettings]: undefined;
+  [BottomTabsScreenTypes.Profile]: undefined;
 };
 
 export type BottomTypeProps = BottomTabNavigationProp<
