98fcde32f6e26f6d4e62da07a68ea93fd7a4a8c6
stepan103ua <stepan8877ua@gmail.com>
2022-11-15 01:32:14 +0200

    feat: add money transfer card carousel

---
+++ src/screens/MoneyTransferScreen/TransferCardCarousel/TransferCardCarouselItem/transferCardCarouselItem.settings.ts
@@ -0,0 +1,16 @@
+import { colors } from '../../../../constants/colors';
+
+export const providerIconName: { [key: string]: string } = {
+  ['Visa']: 'cc-visa',
+  ['Mastercard']: 'cc-mastercard',
+};
+
+export const currencies: { [key: string]: string } = {
+  ['UAH']: '₴',
+  ['USD']: '$',
+  ['EUR']: '€',
+};
+
+export const iconSize = 40;
+
+export const iconColor = colors.black;
