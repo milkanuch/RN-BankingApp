7d56b8609ff1a620a99d699c26ee4218cbaf6de8
Xaiodo <rostislavgovbak@gmail.com>
2022-11-15 16:59:07 +0200

    refactor: title text component

--- src/components/TitleText/TitleText.tsx
+++ src/components/TitleText/TitleText.tsx
@@ -4,11 +4,11 @@ import React, { FC } from 'react';
 import { ITitleTextProps } from './titleText.types';
 import styles from './titleText.styles';
 
-const TitleText: FC<ITitleTextProps> = ({ text, subtitle }) => {
-  const textStyle = subtitle ? styles.subtitleText : styles.titleText;
+const TitleText: FC<ITitleTextProps> = ({ textStyle, text, subtitle }) => {
+  const selectedTextStyle = subtitle ? styles.subtitleText : styles.titleText;
   return (
     <View style={[styles.container, !subtitle && styles.titleContainer]}>
-      <Text style={textStyle}>{text}</Text>
+      <Text style={[selectedTextStyle, textStyle]}>{text}</Text>
     </View>
   );
 };
--- src/components/TitleText/titleText.types.ts
+++ src/components/TitleText/titleText.types.ts
@@ -1,4 +1,7 @@
+import { ViewStyle } from 'react-native';
+
 export interface ITitleTextProps {
   text: string;
   subtitle?: boolean;
+  textStyle?: ViewStyle;
 }
