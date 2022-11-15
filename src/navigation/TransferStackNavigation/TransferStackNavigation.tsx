58876ec065ddf75db0ae641a4aaa37c2f8fd84a3
stepan103ua <stepan8877ua@gmail.com>
2022-11-15 22:25:07 +0200

    refactor: refactor transfer status in stack

--- src/navigation/TransferStackNavigation/TransferStackNavigation.tsx
+++ src/navigation/TransferStackNavigation/TransferStackNavigation.tsx
@@ -1,8 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import MoneyTransferScreen from '../../screens/MoneyTransferScreen/MoneyTransferScreen';
+import TransactionStatusScreen from '../../screens/TransactionStatusScreen/TransactionStatusScreen';
 
-import { TRANSFER_SCREEN_OPTION } from './transferStackNavigation.settings';
+import {
+  TRANSFER_SCREEN_OPTION,
+  TRANSFER_STATUS_SCREEN_OPTION,
+} from './transferStackNavigation.settings';
 
 import {
   TransferStackParamsList,
@@ -18,6 +22,11 @@ const TransferStackNavigation = () => {
         name={TransferStackScreenTypes.Transfer}
         component={MoneyTransferScreen}
         options={TRANSFER_SCREEN_OPTION}
+      />
+      <Stack.Screen
+        name={TransferStackScreenTypes.TransferStatus}
+        component={TransactionStatusScreen}
+        options={TRANSFER_STATUS_SCREEN_OPTION}
       />
     </Stack.Navigator>
   );
