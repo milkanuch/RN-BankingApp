769a803ca91ee11bf14279fa4f77df205ac224c2
Xaiodo <rostislavgovbak@gmail.com>
2022-11-15 16:43:29 +0200

    feat: created deposit withdrawls section item

---
+++ src/components/DepositWithdrawlsSection/DepositWithdrawlsSectionItem/DepositWithdrawlsSectionItem.tsx
@@ -0,0 +1,30 @@
+import { View, Text } from 'react-native';
+import React, { FC } from 'react';
+
+import { TouchableHighlight } from 'react-native-gesture-handler';
+
+import styles from './depositWithdrawlsSectionItem.styles';
+import { IDepositWithdrawlsSectionItemProps } from './depositWithdrawlsSectionItem.types';
+
+const DepositWithdrawlsSectionItem: FC<IDepositWithdrawlsSectionItemProps> = ({
+  cardProvider,
+  currency,
+  date,
+  total,
+}) => {
+  return (
+    <TouchableHighlight>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Text style={styles.cardProvider}>{cardProvider}</Text>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+        <View style={styles.money}>
+          <Text style={styles.total}>-{total}</Text>
+          <Text style={styles.currency}>{currency}</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+export default DepositWithdrawlsSectionItem;
