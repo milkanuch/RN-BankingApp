e02564a95a402b0d14fa09902f449a84c987e86f
stepan103ua <stepan8877ua@gmail.com>
2022-11-15 22:21:47 +0200

    refactor: remove consol log

--- src/components/FrequentlyUsedFunctions/FrequentlyUsedButtons/frequentlyUsedButtons.settings.ts
+++ src/components/FrequentlyUsedFunctions/FrequentlyUsedButtons/frequentlyUsedButtons.settings.ts
@@ -1,14 +1,14 @@
 export const transferButtonTitle = 'Transfer';
 export const transferButtonIcon = 'arrowup';
-export const transferButtonOnPress = () => console.log('Transfer');
+export const transferButtonOnPress = () => {};
 
 export const depositButtonTitle = 'Deposit';
 export const depositButtonIcon = 'arrowdown';
-export const depositButtonOnPress = () => console.log('Deposit');
+export const depositButtonOnPress = () => {};
 
 export const detailsButtonTitle = 'Details';
 export const detailsButtonIcon = 'bars';
-export const detailsButtonOnPress = () => console.log('Details');
+export const detailsButtonOnPress = () => {};
 
 export const buttons = [
   {
