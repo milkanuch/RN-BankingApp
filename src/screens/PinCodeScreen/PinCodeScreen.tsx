1789742720819e3e10543783af9dfc6c4d72fa84
milkanuch <milkanic2@gmail.com>
2022-11-16 15:23:46 +0200

    feat: add pincode flow

--- src/screens/PinCodeScreen/PinCodeScreen.tsx
+++ src/screens/PinCodeScreen/PinCodeScreen.tsx
@@ -2,6 +2,8 @@ import React, { FC, useCallback, useEffect, useState } from 'react';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { ActivityIndicator, View } from 'react-native';
+
 import {
   AuthStackScreenTypes,
   PinCodeScreenProps,
@@ -16,6 +18,10 @@ import { useFingerPrint } from '../../hooks/useFingerPrint';
 import { useShakeAnimation } from '../../hooks/useShakeAnimation';
 
 import { getItem, setItem } from '../../store/bankStore/store';
+
+import { useUserRefreshMutation } from '../../services';
+
+import useRefreshToken from '../../hooks/useRefreshToken';
 
 import { PinCodeContainer } from './PinCodeContainer/PinCodeContainer';
 import PinCodeFooter from './PinCodeFooter/PinCodeFooter';
@@ -32,24 +38,31 @@ const PinCodeScreen: FC<PinCodeScreenProps> = ({ navigation }) => {
 
   const [pinCode, setPinCode] = useState<string[]>([]);
   const [confirmationPin, setConfirmationPin] = useState<string[]>([]);
-  const [isRegister, setIsRegister] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [refresh] = useUserRefreshMutation();
 
   const { animation, shake } = useShakeAnimation();
   const { onFingerprint } = useFingerPrint();
+  const { refreshToken } = useRefreshToken();
 
   const fetchPinCode = useCallback(async () => {
     const tempCode = await getItem('PinCode');
-    if (tempCode) {
+    const tempToken = await getItem('RefreshToken');
+
+    if (tempCode && tempToken) {
       setPinCode([...tempCode]);
-      setIsRegister(false);
+      setIsLogin(false);
     }
+    setIsLoaded(true);
   }, []);
 
   const tryLogin = useCallback(() => {
     if (confirmationPin.length === PIN_CODE_LENGTH) {
       if (isEqual(pinCode, confirmationPin)) {
         dispatch(setUserIsLogged(true));
-        if (isRegister) {
+        refresh({ refreshToken });
+        if (isLogin) {
           setItem('PinCode', confirmationPin.join(''));
         }
       } else {
@@ -57,15 +70,34 @@ const PinCodeScreen: FC<PinCodeScreenProps> = ({ navigation }) => {
         shake();
       }
     }
-  }, [confirmationPin, pinCode, isRegister, dispatch, shake]);
+  }, [
+    confirmationPin,
+    pinCode,
+    isLogin,
+    dispatch,
+    shake,
+    refresh,
+    refreshToken,
+  ]);
 
   useEffect(() => {
     fetchPinCode();
-  }, [fetchPinCode]);
+    if (isLoaded && !isLogin) {
+      onFingerprint();
+    }
+  }, [fetchPinCode, onFingerprint, isLoaded, isLogin]);
 
   useEffect(() => {
     tryLogin();
   }, [confirmationPin, tryLogin]);
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
 
   const addNumber = (num: string) => {
     if (pinCode.length < PIN_CODE_LENGTH) {
@@ -91,12 +123,14 @@ const PinCodeScreen: FC<PinCodeScreenProps> = ({ navigation }) => {
     addNumber,
     removeNumber,
     onFingerprint,
+    isTouchIdDisabled: isLogin,
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <PinCodeHeader />
       <PinCodeContainer
+        isLogin={(pinCode.length < PIN_CODE_LENGTH && isLogin) || !isLogin}
         pinCode={pinCode.length === PIN_CODE_LENGTH ? confirmationPin : pinCode}
         animation={animation}
       />
