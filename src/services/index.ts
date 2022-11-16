e3ac25b42e79f5c29c7aa01fc212c0872195c384
milkanuch <milkanic2@gmail.com>
2022-11-16 15:48:04 +0200

    feat: add prepare headers

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,112 +1,134 @@
 import { Config } from 'react-native-config';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { getItem } from './../store/bankStore/store';
+
 import {
-  AllCardsResponseParams,
-  NewCardParams,
-  NewCardResponseParams,
-  TransactionParams,
-  TransactionResponseParams,
-  UserLoginParams,
-  UserParams,
-  UserRefreshParams,
-  UserResponseParams,
+  IAllCardsResponseParams,
+  INewCardParams,
+  INewCardResponseParams,
+  ITransactionParams,
+  ITransactionResponseParams,
+  IUserInfoResponseParamas,
+  IUserLoginParams,
+  IUserParams,
+  IUserRefreshParams,
+  IUserResponseParams,
 } from './bankApi.types';
+
+const anonymousEndpoints = [
+  'userLogin',
+  'userRegister',
+  'userRefresh',
+  'userLogout',
+];
 
 export const bankApi = createApi({
   reducerPath: 'bankApi',
   tagTypes: ['Cards'],
   baseQuery: fetchBaseQuery({
     baseUrl: Config.API_URL,
+    prepareHeaders: async (headers, { endpoint }) => {
+      const token = await getItem('AccessToken');
+      if (token && !anonymousEndpoints.includes(endpoint)) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   endpoints: builder => ({
-    userRegister: builder.mutation<UserResponseParams, UserParams>({
+    userRegister: builder.mutation<IUserResponseParams, IUserParams>({
       query: body => ({
         url: '/register',
         method: 'POST',
-        body: body,
+        body,
         headers: {
           'Content-type': 'application/json',
         },
       }),
     }),
-    userLogin: builder.mutation<UserResponseParams, UserLoginParams>({
+    userLogin: builder.mutation<IUserResponseParams, IUserLoginParams>({
       query: body => ({
         url: '/login',
         method: 'POST',
-        body: body,
+        body,
         headers: {
           'Content-type': 'application/json',
         },
       }),
     }),
-    newCard: builder.mutation<NewCardResponseParams, NewCardParams>({
-      query: ({ accessToken, currency, provider, type }) => ({
-        url: '/user/newcard',
+    userRefresh: builder.mutation<IUserResponseParams, IUserRefreshParams>({
+      query: body => ({
+        url: '/refresh_token',
+        method: 'POST',
+        body,
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }),
+    }),
+    userInfo: builder.query<IUserInfoResponseParamas, void>({
+      query: () => ({
+        url: '/user/info',
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }),
+    }),
+    userLogout: builder.mutation<IUserResponseParams, IUserRefreshParams>({
+      query: body => ({
+        url: '/refresh_token/logout',
+        method: 'POST',
+        body,
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }),
+    }),
+    newCard: builder.mutation<INewCardResponseParams, INewCardParams>({
+      query: ({ currency, provider, type }) => ({
+        url: '/user/cards/new',
         method: 'POST',
         body: { currency, provider, type },
         headers: {
           'Content-type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
         },
       }),
     }),
-    userRefresh: builder.query<UserResponseParams, UserRefreshParams>({
-      query: body => ({
-        url: '/refresh_token',
+    transaction: builder.mutation<
+      ITransactionResponseParams,
+      ITransactionParams
+    >({
+      query: ({
+        senderCardNumber,
+        receiverCardNumber,
+        receiverName,
+        sum,
+        purpose,
+      }) => ({
+        url: '/user/transactions/new',
         method: 'POST',
-        body: body,
-        headers: {
-          'Content-type': 'application/json',
-        },
-      }),
-    }),
-    userLogout: builder.query<UserResponseParams, UserRefreshParams>({
-      query: body => ({
-        url: '/refresh_token/logout',
-        method: 'POST',
-        body: body,
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: 'Bearer',
-        },
-      }),
-    }),
-    transaction: builder.mutation<TransactionResponseParams, TransactionParams>(
-      {
-        query: ({
-          accessToken,
+        body: {
           senderCardNumber,
           receiverCardNumber,
           receiverName,
           sum,
           purpose,
-        }) => ({
-          url: '/user/transactions/new',
-          method: 'POST',
-          body: {
-            senderCardNumber,
-            receiverCardNumber,
-            receiverName,
-            sum,
-            purpose,
-          },
-          headers: {
-            'Content-type': 'application/json',
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }),
-        invalidatesTags: ['Cards'],
-      },
-    ),
-    getAllCards: builder.query<AllCardsResponseParams, String>({
-      query: accessToken => {
+        },
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }),
+      invalidatesTags: ['Cards'],
+    }),
+    getAllCards: builder.query<IAllCardsResponseParams, void>({
+      query: () => {
         return {
           url: '/user/cards/',
           method: 'GET',
           headers: {
             'Content-type': 'application/json',
-            Authorization: `Bearer ${accessToken}`,
           },
         };
       },
@@ -127,6 +149,9 @@ export const bankApi = createApi({
 export const {
   useUserRegisterMutation,
   useUserLoginMutation,
+  useUserInfoQuery,
+  useUserRefreshMutation,
+  useUserLogoutMutation,
   useNewCardMutation,
   useTransactionMutation,
   useGetAllCardsQuery,
