5c732f9c94817544ec1cbeb353bee8bf6aa46f4f
milkanuch <milkanic2@gmail.com>
2022-11-17 00:31:57 +0200

    feat: create app loading screen

---
+++ src/screens/AppLoadingScreen/appLoadingScreen.styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from '../../constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loader: {
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+    borderWidth: 15,
+    position: 'absolute',
+  },
+  dark: {
+    shadowColor: colors.black,
+    borderColor: colors.black,
+  },
+  light: {
+    backgroundColor: colors.black,
+    shadowColor: colors.white,
+    borderColor: colors.white,
+  },
+});
+
+export default styles;
---
+++ src/screens/AppLoadingScreen/appLoadingScreen.types.ts
@@ -0,0 +1,8 @@
+export interface IAppLoadingScreen {
+  theme?: AppLoadingScreenTheme;
+}
+
+export enum AppLoadingScreenTheme {
+  Dark = 'dark',
+  Light = 'light',
+}
