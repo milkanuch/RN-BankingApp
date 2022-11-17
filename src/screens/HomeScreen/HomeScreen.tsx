2b83daceb357c133d595cca8cb19bf3b07172372
Artem Magei <artemmagej@gmail.com>
2022-11-17 13:09:40 +0200

    feat: construct home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,32 +1,55 @@
 import React from 'react';
-
-import { ScrollView } from 'react-native';
+import { RefreshControl, ScrollView, Text } from 'react-native';
 
 import CardCaroules from '../../components/CardCarousel/CardCarousel';
 import FrequentlyUsedFunctions from '../../components/FrequentlyUsedFunctions/FrequentlyUsedFunctions';
-
 import DepositWithdrawlsSection from '../../components/DepositWithdrawlsSection/DepositWithdrawlsSection';
-
 import {
   useGetAllCardsQuery,
   useGetAllUserTransactionsQuery,
 } from '../../services';
-
 import AppLoadingScreen from '../AppLoadingScreen/AppLoadingScreen';
 
 import styles from './homeScreen.styles';
+import { homeTitle } from './homeScreen.settings';
 
 const HomeScreen = () => {
-  const { data: cardsData, isLoading: fetchingCards } = useGetAllCardsQuery();
-  const { data: transactionsData, isLoading: fetchingTransactions } =
-    useGetAllUserTransactionsQuery();
+  const {
+    data: cardsData,
+    isLoading: fetchingCards,
+    refetch: cardsRefetch,
+    isFetching: isCardsRefatching,
+  } = useGetAllCardsQuery();
+
+  const {
+    data: transactionsData,
+    isLoading: fetchingTransactions,
+    refetch: refetchingTransactions,
+    isFetching: isRefetchingTransactions,
+  } = useGetAllUserTransactionsQuery();
+
+  const onRefresh = () => {
+    refetchingTransactions();
+    cardsRefetch();
+  };
+
+  const refreshControl = (
+    <RefreshControl
+      refreshing={isRefetchingTransactions && isCardsRefatching}
+      onRefresh={onRefresh}
+    />
+  );
 
   if (fetchingCards || fetchingTransactions) {
     return <AppLoadingScreen />;
   }
 
   return (
-    <ScrollView style={styles.screen} nestedScrollEnabled>
+    <ScrollView
+      style={styles.screen}
+      nestedScrollEnabled
+      refreshControl={refreshControl}>
+      <Text style={styles.title}>{homeTitle}</Text>
       <CardCaroules cards={cardsData?.ok!} showIndicator />
       <FrequentlyUsedFunctions />
       <DepositWithdrawlsSection transactions={transactionsData!} />
