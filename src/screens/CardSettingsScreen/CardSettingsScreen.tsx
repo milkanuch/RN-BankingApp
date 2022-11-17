a3ace1927f86a3c3f97f7a2dac6fe38d2a8bba02
milkanuch <milkanic2@gmail.com>
2022-11-18 01:29:43 +0200

    feat: create card settings screen

--- src/screens/CardSettingsScreen/CardSettingsScreen.tsx
+++ src/screens/CardSettingsScreen/CardSettingsScreen.tsx
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import CardCaroules from '../../components/CardCarousel/CardCarousel';
+
+import { useGetAllCardsQuery } from '../../services';
+import AppLoadingScreen from '../AppLoadingScreen/AppLoadingScreen';
+
+import styles from './cardSettingsScreen.styles';
+import CardSettingsContent from './CardSettingsContent/CardSettingsContent';
 
 export default function CardSettingsScreen() {
+  const [selectedCard, setSelectedCard] = useState(0);
+  const { data, isLoading } = useGetAllCardsQuery();
+
+  if (isLoading) {
+    return <AppLoadingScreen />;
+  }
+
   return (
-    <View>
-      <Text>CardSettingsScreen</Text>
-    </View>
+    <SafeAreaView style={styles.scren}>
+      <Text style={styles.cardSettingsTitle}>Card</Text>
+      <CardCaroules
+        cards={data?.ok!}
+        showIndicator
+        onChange={setSelectedCard}
+      />
+      {data && selectedCard !== data.ok.length ? (
+        <CardSettingsContent
+          cardNumber={data?.ok[selectedCard].cardNumber}
+          expirationDate={data?.ok[selectedCard].expirationTime}
+          securityCode={data?.ok[selectedCard].cvvCode}
+        />
+      ) : (
+        <View>
+          <Text style={styles.cardSettingsTitle}>There's no information</Text>
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
---
+++ src/screens/CardSettingsScreen/cardSettingsScreen.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from '../../constants/colors';
+
+const styles = StyleSheet.create({
+  scren: {
+    flex: 1,
+  },
+  cardSettingsTitle: {
+    fontSize: 26,
+    color: colors.black,
+    marginLeft: 20,
+    marginTop: 5,
+    fontFamily: 'e-Ukraine-Light',
+  },
+});
+
+export default styles;
