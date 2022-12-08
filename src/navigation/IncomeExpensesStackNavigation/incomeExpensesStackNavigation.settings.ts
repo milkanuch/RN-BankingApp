fa7a36d4da9e7db5ee4af700c50886b0584a0f63
milkanuch <milkanic2@gmail.com>
2022-12-08 17:53:12 +0200

    feat: add app header component

--- src/navigation/IncomeExpensesStackNavigation/incomeExpensesStackNavigation.settings.ts
+++
@@ -1,5 +0,0 @@
-import { StackNavigationOptions } from '@react-navigation/stack';
-
-export const INCOME_EXPENSES_SCREEN_OPTION: StackNavigationOptions = {
-  headerShown: false,
-};
---
+++ src/navigation/IncomeExpensesStackNavigation/incomeExpensesStackNavigation.settings.tsx
@@ -0,0 +1,14 @@
+import { StackNavigationOptions } from '@react-navigation/stack';
+import React from 'react';
+
+import AppHeader from '../../components/AppHeader/AppHeader';
+
+import { colors } from '../../constants/colors';
+
+export const INCOME_EXPENSES_SCREEN_OPTION: StackNavigationOptions = {
+  headerShadowVisible: false,
+  headerTitle: () => <AppHeader />,
+  headerStyle: {
+    backgroundColor: colors.white,
+  },
+};
