7e49bbba18d49c999a6cdee0312eb9b9b268ffa9
milkanuch <milkanic2@gmail.com>
2022-12-08 18:00:23 +0200

    feat: add income expenses card

---
+++ src/screens/IncomeExpensesScreen/IncomeExpensesCard/IncomeExpensesCard.tsx
@@ -0,0 +1,34 @@
+import { View, Text } from 'react-native';
+import React, { FC } from 'react';
+
+import ProgressBar from '../../../components/ProgressBar/ProgressBar';
+
+import { currencies } from '../../MoneyTransferScreen/TransferCardCarousel/TransferCardCarouselItem/transferCardCarouselItem.settings';
+
+import { IIncomeExpensesCardProps } from './incomeExpensesCard.types';
+import styles from './incomeExpensesCard.styles';
+import { numberOfLines } from './incomeExpensesCard.settings';
+
+const IncomeExpensesCard: FC<IIncomeExpensesCardProps> = ({
+  category,
+  currency,
+  totalSpendPerMonth,
+  totalSum,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={styles.categoryTitle}
+        adjustsFontSizeToFit
+        numberOfLines={numberOfLines}>
+        {category}
+      </Text>
+      <Text style={styles.totalSpendTitle}>
+        {totalSpendPerMonth + currencies[currency]}
+      </Text>
+      <ProgressBar progressLimit={totalSum} progressLine={totalSpendPerMonth} />
+    </View>
+  );
+};
+
+export default IncomeExpensesCard;
---
+++ src/screens/IncomeExpensesScreen/IncomeExpensesCard/incomeExpensesCard.settings.ts
@@ -0,0 +1,1 @@
+export const numberOfLines = 1;
