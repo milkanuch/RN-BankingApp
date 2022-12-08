8f5051d0d3870af6f012fb09577f21f53dbeaef2
milkanuch <milkanic2@gmail.com>
2022-12-08 17:59:35 +0200

    feat: add center label component

---
+++ src/screens/IncomeExpensesScreen/IncomeExpensesPieChart/CenterLabelComponent/CenterLabelComponent.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import React, { FC } from 'react';
+
+import { currencies } from '../../../MoneyTransferScreen/TransferCardCarousel/TransferCardCarouselItem/transferCardCarouselItem.settings';
+
+import styles from './centerLabelComponent.styles';
+import { ICenterLabelProps } from './centerLabelComponent.types';
+
+const CenterLabel: FC<ICenterLabelProps> = ({ sum, currency, month, year }) => {
+  return (
+    <View style={styles.centerLabelContainer}>
+      <Text style={styles.monthText}>{month}</Text>
+      <Text style={styles.yearText}>{year}</Text>
+      <Text style={styles.totalSpendText}>{sum + currencies[currency]}</Text>
+    </View>
+  );
+};
+
+export default CenterLabel;
---
+++ src/screens/IncomeExpensesScreen/IncomeExpensesPieChart/CenterLabelComponent/centerLabelComponent.types.ts
@@ -0,0 +1,6 @@
+export interface ICenterLabelProps {
+  sum: number;
+  currency: string;
+  month: string;
+  year: string;
+}
